819704963230febab226789f210867086fa44339
Granhelios <granhelios@gmail.com>
2022-01-30 13:30:06 -0300

    Eliminando comentarios

--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,6 @@ use App\Http\Controllers\CalleController;
 
 
 
-
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 //inicio de todo el Route 
 
 
